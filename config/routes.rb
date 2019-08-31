22989dbc2721bde58c42e0e9baf61296ce2e5540
Miguel Salgueiro <thekkam@outlook.com>
2019-08-31 15:12:13 -0400

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'cars#index'
   resources :cars
-  resources :cars
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +56,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
